3577d213a2e5d89d848eb8e9c36b2f60cd3e1150
Ricardo Gallozzi <92772483+RicJr06@users.noreply.github.com>
2022-10-13 21:13:01 -0300

    Create cliente.java

---
+++ cliente.java
@@ -0,0 +1,12 @@
+public class Cliente {
+    //Atributos
+    String nome;
+    String endereco;
+    
+    //Criar um construtor (Deve ter o mesmo nome da classe)
+    public Cliente(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+    
+}
